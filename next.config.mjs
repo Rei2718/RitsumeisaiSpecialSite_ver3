06cf6c31d7a3c93134760ccc1461a07ed384655e
Rei2718 <once2718@gmail.com>
2024-07-16 07:48:30 +0900

    Img Domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'scontent-nrt1-1.cdninstagram.com', 
+            'scontent.cdninstagram.com',
+            'scontent-nrt1-2.cdninstagram.com'
+        ],
+    },
+};
 
 export default nextConfig;
